7acf7b58f0341bcefc370c8476b1b8ae7b9fa52b
desricha <richa0387@gmail.com>
2014-05-05 06:33:18 +0530

    home controller

--- src/com/dost/controller/HomeController.java
+++ src/com/dost/controller/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+\\richa test\\
+
 @Controller
 public class HomeController {
 
