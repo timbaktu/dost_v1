711b5a7c2f510b32193ab16f57c1b9edfb9ed015
satya <satyajeet1234@gmail.com>
2014-08-23 16:59:14 -0400

    removing unused imports

--- src/com/dost/controller/MessageController.java
+++ src/com/dost/controller/MessageController.java
@@ -7,8 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
