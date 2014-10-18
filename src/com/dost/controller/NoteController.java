fb59015754979791cccef334a5eda138a60e5b4c
satya <satyajeet1234@gmail.com>
2014-10-17 21:44:32 -0400

    saving note

--- src/com/dost/controller/NoteController.java
+++ src/com/dost/controller/NoteController.java
@@ -9,8 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.dost.hibernate.DbMessage;
 import com.dost.hibernate.DbNote;
+import com.dost.hibernate.DbUser;
+import com.dost.model.Note;
+import com.dost.service.MessageService;
 import com.dost.service.NoteService;
+import com.dost.service.UserService;
 
 @Controller
 @RequestMapping("api")
@@ -18,6 +23,12 @@ public class NoteController {
 
 	@Autowired
 	NoteService noteService;
+	
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	MessageService messageService;
 
 	@RequestMapping(value="/msgId/{msgId}/notes/all", method=RequestMethod.GET)  
 	@ResponseBody
@@ -28,7 +39,22 @@ public class NoteController {
 	
 	@RequestMapping(value="/notes/add", method=RequestMethod.POST)  
 	@ResponseBody
-	public DbNote addNote(DbNote note) {
-		return noteService.saveNote(note);
+	public DbNote addNote(Note note) {
+		DbNote dbnote = new DbNote();
+		populateDbNote(dbnote, note);
+		return noteService.saveNote(dbnote);
+	}
+	
+	private void populateDbNote(DbNote dbnote, Note note) {
+		if(note != null) {
+			dbnote.setNoteId(note.getNoteId());
+		}
+		DbUser user = new DbUser();
+		user.setUserId(note.getUserId());
+		dbnote.setUser(userService.getUser(note.getUserId()));
+		
+		DbMessage message = new DbMessage();
+		message.setMessageId(note.getMessageId());
+		dbnote.setMessage(message);
 	}
 }
---
+++ src/com/dost/model/Note.java
@@ -0,0 +1,28 @@
+package com.dost.model;
+
+public class Note {
+
+	private Long noteId;
+	private Long userId;
+	private Long messageId;
+	public Long getNoteId() {
+		return noteId;
+	}
+	public void setNoteId(Long noteId) {
+		this.noteId = noteId;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public Long getMessageId() {
+		return messageId;
+	}
+	public void setMessageId(Long messageId) {
+		this.messageId = messageId;
+	}
+	
+	
+}
