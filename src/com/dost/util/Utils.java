6661b7dc24eb9357ac058da3a4c32b8eebf0facc
snandekar <snandekar@SATYAJEET-N.ne.virtmed.com>
2014-12-10 22:02:30 -0500

    adding time in message/conversation date. Earlier it was just date with no time.

--- src/com/dost/util/Utils.java
+++ src/com/dost/util/Utils.java
@@ -3,17 +3,16 @@ package com.dost.util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 
 public class Utils {
 
 	public static String formatDate(Date date) {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		return dateFormat.format(date);
 	}
 	
 	public static Date formatDate(String dateStr) {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Date date = null;
 		try {
 			date = dateFormat.parse(dateStr);
@@ -42,14 +41,14 @@ public class Utils {
 	public static String unixToDate(String unix_timestamp) {    
 	    long timestamp = Long.parseLong(unix_timestamp) * 1000;
 
-	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	    String date = null;
 	    date = sdf.format(timestamp);
 	    return date.toString();
 	}
 	
 	public static Long dateToUnix(String date) {    
-	    Date localDate = formatDate("yyyy-M-dd hh:mm:ss", date);
+	    Date localDate = formatDate("yyyy-M-dd HH:mm:ss", date);
 	    localDate.setHours(localDate.getHours() - 4);
 //	    return localDate.getTime() / 1000L;
 	    return localDate.getTime();
