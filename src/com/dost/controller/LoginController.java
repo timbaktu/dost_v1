987ec0a7d5f2bbf53e7d2ad88c20036f67d0f290
Satyajeet <satyajeet1234@gmail.com>
2015-02-10 11:46:30 -0500

    Fixing loginController hardcoding

--- src/com/dost/controller/LoginController.java
+++ src/com/dost/controller/LoginController.java
@@ -46,8 +46,6 @@ public class LoginController {
 	public Map<String, String> authenticateUser(HttpServletRequest request) {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		username = "sohil";
-		password = "dost123";
 		DbUser dbUser = userService.authenticateUser(username, password);
 
 		Map<String, String> response = new HashMap<String, String>();
@@ -62,17 +60,17 @@ public class LoginController {
 			
 			// Add this token in HashMap and add it in servletContext which can be used later to check if user has passed proper token 
 			// and for blocking and logging user out user
-			ServletContext servletContext = request.getServletContext();
-			Object object = servletContext.getAttribute("userMap");
-			Map<String, String> userMap;
-			if(object != null) {
-				userMap = (Map<String, String>)object;
-			}
-			else {
-				userMap = new HashMap<String, String>();
-			}
-			userMap.put(token+"", dbUser.getUsername());
-			servletContext.setAttribute("userMap", userMap);
+//			ServletContext servletContext = request.getServletContext();
+//			Object object = servletContext.getAttribute("userMap");
+//			Map<String, String> userMap;
+//			if(object != null) {
+//				userMap = (Map<String, String>)object;
+//			}
+//			else {
+//				userMap = new HashMap<String, String>();
+//			}
+//			userMap.put(token+"", dbUser.getUsername());
+//			servletContext.setAttribute("userMap", userMap);
 		}
 		else {
 			response.put("token", "");
