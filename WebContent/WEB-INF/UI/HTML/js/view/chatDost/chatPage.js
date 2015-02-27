30f30ad1ae44cee0cb1731fad5b9fa85819b7940
Sandeep Kasani <sandeepkasani@gmail.com>
2015-02-27 09:04:52 +0530

    Leave a message to dost

--- WebContent/WEB-INF/UI/HTML/js/view/chatDost/chatPage.js
+++ WebContent/WEB-INF/UI/HTML/js/view/chatDost/chatPage.js
@@ -3,15 +3,19 @@ define([
 	'underscore',
 	'backbone',
 	'hbs!../../template/chatDost/chat',
+    'hbs!../../template/chatDost/composeMsgModal',         
+    'view/basemodal/BaseModal',
 	'utils',
 	'event/dispatcher'
-], function($, _, Backbone, chatDostLayout, Utils, Dispatcher) {
+], function($, _, Backbone, chatDostLayout, ComposeMsgModal, BaseModalView, Utils, Dispatcher) {
 	var chatDostPage = Backbone.View.extend({
 		el: "#main-content",
 		initialize: function() {
 			// body...
 		},
-		events: {},
+		events: {
+			"click .email":"composeMsg"
+		},
 		render: function() {
 			$.ajax("http://localhost:8800/dost/api/counselors/all").done(function(response){
 				$("#main-content").html(chatDostLayout({objects:response}));
@@ -19,6 +23,45 @@ define([
 				$(window).unbind('scroll');
 				$('body').css("padding-top", "114px");
 			});
+		},
+		composeMsg: function(e){
+			e.preventDefault();
+			e.stopPropagation();
+			var self = this;
+			$modalBody = $('<div>').html(ComposeMsgModal());
+			var msgToDost = new BaseModalView({
+                title: "",
+                headerHidden: true,
+                className : 'modal fade compose-message-modal',
+                buttonList: [
+                    ['SEND NOW', function(modal, event){
+                    	var self = this;
+                    	var content = modal.$el.find("textarea").val().replace(/\n/g, '<br/>');
+                    	var subject= modal.$el.find(".subject").val().replace(/\n/g, '<br/>');
+                    	var recipients;
+                    	$.ajax("http://localhost:8800/dost/api/user/"+$(".recipients").val()).done(function(details){
+	                    	var url = "http://localhost:8800/dost/api/user/message?subject="+subject+"&content="+content+"&recipients="+details.userId+"&senderId=" +LoginStatus.get('userId');
+	                    	$.ajax({
+	                    		type: "POST",
+	                    		url: url
+	                    	}).done(function(response){
+	                    		console.log(response);
+	                    		var note = response,
+	                    			userName = response.user.username;
+	                    		$("<div class='notes-info'>").append("<div class='notes-heading'>"+
+	                    				note.note+"</div><div class='notes-date pull-right'>" +
+	                    				userName +" " + Utils.getDateDiff(note.noteDate) + 
+	                    				"</div>").prependTo("#notesContainer");
+	                    		
+	                    	});
+	    				});
+                    	modal.teardown();                    	
+                    }.bind(self), 'option-btn composeMsg-send btn']
+                ],
+                body: $modalBody,
+                //data: usernames
+            });
+			msgToDost.show();
 		}
 	});
 
--- WebContent/WEB-INF/UI/HTML/js/view/yourdost/counselorView.js
+++ WebContent/WEB-INF/UI/HTML/js/view/yourdost/counselorView.js
@@ -2,12 +2,14 @@ define([ 'jquery',
          'backbone',
          'underscore',
          'hbs!../../template/yourdost/counselor',
+         'hbs!../../template/chatDost/composeMsgModal',         
+         'view/basemodal/BaseModal',
          'utils',
          'event/dispatcher',
          'model/login',
          'router/app-router'
          ],
-function($, Backbone, _, counselorTemplate, Utils, Dispatcher, LoginStatus, Router) {
+function($, Backbone, _, counselorTemplate, ComposeMsgModal, BaseModalView, Utils, Dispatcher, LoginStatus, Router) {
 	
 	var CounselorView = Backbone.View.extend({
 		template : counselorTemplate,
@@ -17,8 +19,44 @@ function($, Backbone, _, counselorTemplate, Utils, Dispatcher, LoginStatus, Rout
 			'click .message-btn': 'message',
 			'click .detail-btn': 'openDetail'
 		},
-		message: function(){
-			console.log("message Clicked");
+		message: function(e){
+			e.preventDefault();
+			e.stopPropagation();
+			var self = this;
+			$modalBody = $('<div>').html(ComposeMsgModal());
+			var msgToDost = new BaseModalView({
+                title: "",
+                headerHidden: true,
+                className : 'modal fade compose-message-modal',
+                buttonList: [
+                    ['SEND NOW', function(modal, event){
+                    	var self = this;
+                    	var content = modal.$el.find("textarea").val().replace(/\n/g, '<br/>');
+                    	var subject= modal.$el.find(".subject").val().replace(/\n/g, '<br/>');
+                    	var recipients;
+                    	$.ajax("http://localhost:8800/dost/api/user/"+$(".recipients").val()).done(function(details){
+	                    	var url = "http://localhost:8800/dost/api/user/message?subject="+subject+"&content="+content+"&recipients="+details.userId+"&senderId=" +LoginStatus.get('userId');
+	                    	$.ajax({
+	                    		type: "POST",
+	                    		url: url
+	                    	}).done(function(response){
+	                    		console.log(response);
+	                    		var note = response,
+	                    			userName = response.user.username;
+	                    		$("<div class='notes-info'>").append("<div class='notes-heading'>"+
+	                    				note.note+"</div><div class='notes-date pull-right'>" +
+	                    				userName +" " + Utils.getDateDiff(note.noteDate) + 
+	                    				"</div>").prependTo("#notesContainer");
+	                    		
+	                    	});
+	    				});
+                    	modal.teardown();                    	
+                    }.bind(self), 'option-btn composeMsg-send btn']
+                ],
+                body: $modalBody,
+                //data: usernames
+            });
+			msgToDost.show();
 		},
 		initialize : function(options) {
 			var self = this;
